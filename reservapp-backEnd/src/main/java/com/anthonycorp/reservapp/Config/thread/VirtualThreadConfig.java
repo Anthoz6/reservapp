9b53c213f0d08623ff8cc1b35c49bbab9452c754
Anthoz6 <amestrar21@gmail.com>
2025-06-30 10:59:42 -0500

    Adding Project Loom

---
+++ reservapp-backEnd/src/main/java/com/anthonycorp/reservapp/Config/thread/VirtualThreadConfig.java
@@ -0,0 +1,22 @@
+package com.anthonycorp.reservapp.Config.thread;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.Executors;
+
+@Configuration
+public class VirtualThreadConfig {
+
+    @Bean
+    public java.util.concurrent.Executor virtualThreadExecutor() {
+        return Executors.newVirtualThreadPerTaskExecutor();
+    }
+
+    @Bean
+    public TomcatProtocolHandlerCustomizer<?> protocoloHandlerVirtualThread() {
+        return protocolHandler -> protocolHandler.setExecutor(virtualThreadExecutor());
+    }
+
+}
